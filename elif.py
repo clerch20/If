43f27cc545dc284537571d4ad5854099c7479518
clerch20 <43791906+clerch20@users.noreply.github.com>
2018-11-30 12:46:15 -0600

    Add files via upload

---
+++ elif.py
@@ -0,0 +1,37 @@
+import RoboPiLib as RPL
+import setup
+x = 1
+L = Pin 
+R = Pin
+LS = Pin
+RS = Pin
+RPL.pinMode(LS,RPL.INPUT)
+RPL.pinMode(RS,RPL.INPUT)
+
+def left():
+     if readingL == 0:
+          RPL.servoWrite(L,1410)
+     elif readingL == 1:
+	  RPL.servoWrite(L,1500)
+def right():
+     if readingL == 0:
+          RPL.servoWrite(R,1590)
+     Elif readingL == 1:
+	  RPL.servoWrite(R,1500)
+def both():
+     RPL.servoWrite(L,1590)
+     RPL.servoWrite(R,1410)
+def none():
+     RPL.servoWrite(L,1500)
+     RPL.servoWrite(R,1500)
+while x == 1:
+     readingL = RPL.digitalRead(LS)
+     readingR = RPL.digitalRead(RS)
+     if readingL == 0 and readingR == 0:
+          both()
+     elif readingL == 1 and readingR == 1:
+          none()
+     else:
+          left()
+          right()
+
